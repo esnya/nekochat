7124bb28c0cb7f77cc283d0b35632f4e278a4392
ukatama <dev.ukatama@gmail.com>
2016-01-18 22:20:15 +0900

    Remove unused PHP files

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-$html = file_get_contents('index.php');
-$script = implode(array_map(function ($src) {
-    return "<script src=$src></script>";
-}, array_merge(['http://code.jquery.com/qunit/qunit-1.15.0.js'], glob('test/*.js'))));
-echo str_replace('</body>', $script . '</body>', 
-    str_replace('</head>', '<link rel=stylesheet href="http://code.jquery.com/qunit/qunit-1.15.0.css"></head>',
-    str_replace('<body>', '<body><div id=qunit></div><div id=qunit-fixture></div>', $html)));
