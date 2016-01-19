dc8ef49f472cc06c3dadb3e655b054d156009053
ukatama <dev.ukatama@gmail.com>
2016-01-19 14:14:36 +0900

    Migrate from gulp-express to gulp-live-server

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,14 @@ const browserify = require('browserify');
 const fs = require('fs');
 const gulp = require('gulp');
 const babel = require('gulp-babel');
-const express = require('gulp-express');
+const liveserver = require('gulp-live-server');
 const sourcemaps = require('gulp-sourcemaps');
 const gutil = require('gulp-util');
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
 const watchify = require('watchify');
+
+let server = liveserver.new('.');
 
 gulp.task('default', ['build']);
 
@@ -17,12 +19,12 @@ gulp.task('build', ['build:server', 'build:browser']);
 gulp.task('build:server', ['babel']);
 gulp.task('build:browser', ['browserify']);
 
-gulp.task('serve', ['express']);
+gulp.task('serve', ['server']);
 
 gulp.task('watch', ['watch:server', 'watch:browser']);
-gulp.task('watch:server', ['express'], () => {
-    gulp.watch(['src/**/*', '!src/browser/**/*'], ['babel', express.run]);
-    gulp.watch(['dist/**/*'], ['express:notify']);
+gulp.task('watch:server', ['server'], () => {
+    gulp.watch(['src/**/*', '!src/browser/**/*'], ['server']);
+    gulp.watch(['dist/**/*'], file => server.notify(file));
 });
 gulp.task('watch:browser', ['watchify'], () =>
     gulp.watch(['src/**/*', '!src/server/**/*'], ['watchify']));
@@ -83,8 +85,7 @@ w.on('log', gutil.log);
 gulp.task('watchify', ['babel'], bundle(w));
 gulp.task('browserify', ['babel'], bundle(browserify(BrowserifyConfig)));
 
-gulp.task('express', ['babel'], () => express.run(['.']));
-gulp.task('express:notify', next => {
-    express.notify();
+gulp.task('server', ['babel'], next => {
+    server.start();
     next();
 });
