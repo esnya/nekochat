600e242ef0fab16591f2c1231f25d8cc8ea4afe7
ukatama <dev.ukatama@gmail.com>
2016-01-22 21:32:26 +0900

    Apply theme

--- src/browser/components/App.js
+++ src/browser/components/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import { Styles } from 'material-ui';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import Theme from '../theme';
 import { Router } from './Router';
 
 const RouterContainer = connect(state => ({ ...state.route }))(Router);
 
-export const App = (props) => {
-    return <RouterContainer />;
-};
+console.log(Theme)
+export class App extends Component {
+    static get childContextTypes() {
+        return {
+            muiTheme: PropTypes.object,
+        };
+    }
+
+    getChildContext() {
+        return {
+            muiTheme: Styles.ThemeManager.getMuiTheme(Theme),
+        };
+    }
+
+    render() {
+        return <RouterContainer />;
+    }
+}
