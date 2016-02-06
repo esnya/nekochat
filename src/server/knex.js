914bf651001942adfbe49de3962c77599829d273
ukatama <dev.ukatama@gmail.com>
2016-02-06 11:10:46 +0900

    Update table migration

--- src/server/knex.js
+++ src/server/knex.js
@@ -22,30 +22,49 @@ Promise.all([
         table.timestamp('created').notNullable().defaultTo(knex.fn.now());
         table.timestamp('modified').notNullable().defaultTo(knex.fn.now());
         table.timestamp('deleted');
+    })
+    .then(() => {
+        if (config.get('app.guest')) {
+            return knex('users').where('id', 'guest')
+                .first()
+                .then((user) => {
+                    if (!user) {
+                        return knex('users').insert({
+                            id: 'guest',
+                            name: 'Guest',
+                        });
+                    }
+                });
+        }
     }),
     knex.schema.createTableIfNotExists('rooms', (table) => {
         table.string('id').primary();
         table.string('title').notNullable();
-        table.string('user_id').notNullable();
+        table.string('user_id').notNullable()
+            .references('id').inTable('users');
         table.timestamp('created').notNullable().defaultTo(knex.fn.now());
         table.timestamp('modified').notNullable().defaultTo(knex.fn.now());
         table.timestamp('deleted').defaultTo(null);
     }),
     knex.schema.createTableIfNotExists('messages', (table) => {
         table.increments('id').primary();
-        table.string('room_id').notNullable();
-        table.string('user_id').notNullable();
+        table.string('room_id').notNullable()
+            .references('id').inTable('rooms');
+        table.string('user_id').notNullable()
+            .references('id').inTable('users');
+        table.string('icon_id')
+            .references('id').inTable('icons');
         table.string('name').notNullable();
         table.string('message').notNullable();
         table.string('character_url');
-        table.string('icon_id');
         table.timestamp('created').notNullable().defaultTo(knex.fn.now());
         table.timestamp('modified').notNullable().defaultTo(knex.fn.now());
         table.timestamp('deleted').defaultTo(null);
     }),
     knex.schema.createTableIfNotExists('icons', (table) => {
         table.string('id').primary();
-        table.string('user_id').notNullable();
+        table.string('user_id').notNullable()
+            .references('id').inTable('users');
         table.string('name').notNullable();
         table.string('type').notNullable();
         table.binary('data').notNullable();
