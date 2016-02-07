aa34fd48573a3d521b4a84a1030c60021f962389
ukatama <dev.ukatama@gmail.com>
2016-02-07 15:28:30 +0900

    Support redis session store

--- src/server/session.js
+++ src/server/session.js
@@ -1,13 +1,27 @@
 import config from 'config';
+import ConnectRedis from 'connect-redis';
 import Session from 'express-session';
 import KnexSessionStore from 'connect-session-knex';
 import Knex from 'knex';
 
-const Store = KnexSessionStore(Session);
-const knex = Knex(config.get('database.session'));
+const getStore = (type) => {
+    switch(type) {
+        case 'database': {
+            const Store = KnexSessionStore(Session);
+            const knex = Knex(config.get('database.session'));
+            return new Store({knex});
+        }
+        case 'redis': {
+            const RedisStore = ConnectRedis(Session);
+            return new RedisStore(config.get('redis'));
+        }
+        default:
+            throw new Error(`Unsupported store type: ${type}`);
+    }
+};
 
 export const session = Session({
     ...config.get('session'),
     secret: config.get('app.secret'),
-    store: new Store({knex}),
+    store: getStore(config.get('session.store')),
 });
