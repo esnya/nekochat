999a2adc17b500f8083d5471d099f0258ce4638f
ukatama <dev.ukatama@gmail.com>
2016-04-05 07:32:10 +0900

    Fix test

--- src/server/__tests__/test-io.js
+++ src/server/__tests__/test-io.js
@@ -1,11 +1,15 @@
-jest.mock('http');
-jest.dontMock('config');
 describe('io', () => {
+    jest.mock('http');
+    jest.setMock('config', {
+        get: jest.fn((path) => ({
+            'session.store': 'database',
+        }[path] || {})),
+    });
     const io = jest.fn().mockReturnValue({
         use: jest.fn(),
         on: jest.fn(),
     });
     jest.setMock('socket.io', io);
-    jest.dontMock('../io');
+    jest.unmock('../io');
     require('../io');
 });
