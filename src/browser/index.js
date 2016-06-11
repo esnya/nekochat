0fe869a1ea9343c8c0d391873bf35a4383b0fd81
ukatama <dev.ukatama@gmail.com>
2016-06-12 02:13:03 +0900

    Add performance debug tools

--- src/browser/index.js
+++ src/browser/index.js
@@ -1,4 +1,14 @@
+/* eslint global-require: 0 */
+
 require('react-tap-event-plugin')();
 require('../components');
 require('./router').run();
 require('./window-event');
+
+if (require('./config').Config.debug) {
+    require('why-did-you-update')
+        .whyDidYouUpdate(require('react'), {
+            exclude: /^(Connect|^TouchRipple|^EnhancedButton)/,
+        });
+    window.Perf = require('react-addons-perf');
+}
